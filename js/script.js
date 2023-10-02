415238dc3e7720f837c560fe0b7ed38991dff732
david pinto <dbpintogonza@gmail.com>
2023-10-02 13:33:20 -0500

    15

--- js/script.js
+++ js/script.js
@@ -5,4 +5,18 @@ let header = document.querySelector('.header');
 header.classList.toggle('sticky',window.scrollY >100);
 };
 
+/*== swiper ==*/
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
